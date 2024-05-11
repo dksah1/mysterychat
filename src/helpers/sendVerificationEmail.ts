7683f0645e19c89e707a43e953d90e9ec2f0bc85
dksah1 <dkboss127126@gmail.com>
2024-05-11 21:21:03 +0545

    next-auth added

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,1 +1,22 @@
 import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function SendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: email,
+      subject: "Mystry message | Verification code",
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+    return { success: false, message: "failed to send verification email" };
+  } catch (emailerror) {
+    console.error("error sending verification email", emailerror);
+    return { success: false, message: "failed to send verification email" };
+  }
+}
